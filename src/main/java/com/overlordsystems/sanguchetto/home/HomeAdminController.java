3456c3afd97ffecc5babf0333c31db2b5d7e5e28
LkrOverlord <menesestapia@gmail.com>
2022-06-19 18:14:26 -0300

    Add HomeAdminController with single method to open homeView

---
+++ src/main/java/com/overlordsystems/sanguchetto/home/HomeAdminController.java
@@ -0,0 +1,18 @@
+package com.overlordsystems.sanguchetto.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeAdminController {
+
+    /*
+    Debería ser un Controller pero de momento solo como sabemos que va a crecer, se deja con RestController.
+     */
+    @RequestMapping("/Sanguchetto/Home")
+    public ModelAndView goToHome(){
+        ModelAndView model = new ModelAndView("/admin/homeView");
+        return model;
+    }
+}
