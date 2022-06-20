fb5a29cb3de2c22962dd971b878cf48f556829c7
LkrOverlord <menesestapia@gmail.com>
2022-06-20 11:40:01 -0300

    implement private attribute ingredientService like autowired to indicate that this variable is dependency injection. In addition i implement getAllIngredients

--- src/main/java/com/overlordsystems/sanguchetto/ingredient/IngredientController.java
+++ src/main/java/com/overlordsystems/sanguchetto/ingredient/IngredientController.java
@@ -1,14 +1,20 @@
 package com.overlordsystems.sanguchetto.ingredient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @RestController
 public class IngredientController {
 
-    @RequestMapping("/sanguchetto/admin/products/ingredient")
-    public ModelAndView getAllIngredients(){
-        return null;
+    @Autowired
+    private IngredientService ingredientService;
+
+    @RequestMapping("/sanguchetto/admin/products/ingredients")
+    public List<Ingredient> getAllIngredients(){
+        return ingredientService.getAllIngredients();
     }
 }
