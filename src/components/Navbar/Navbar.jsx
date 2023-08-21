ceb75884e7573a087bd1d6e9bacc81c8bb788bee
LkrOverlord <menesestapia@gmail.com>
2023-08-21 19:53:54 -0300

    Nav dividido en secciones y con estilos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,37 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Navbar.css';
+import LogoBrand from './logo/LogoBrand';
+import { Col, Row } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
+        <Navbar expand="lg" className="bg-body-tertiary navBar">
+            <Container fluid>
+                <Row className="w-100 no-gutters">
+                    {/* Logo*/}
+                    <Col lg={3} className='d-flex justify-content-center align-items-center'>
+                        <LogoBrand />
+                    </Col>
+
+                    {/*Botones*/}
+                    <Col lg={6} className='d-flex justify-content-center align-items-center'>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="buttonsNav mx-auto">
+                                <Nav.Link href="#home" className='buttonNav'>HOME</Nav.Link>
+                                <Nav.Link href="#link" className='buttonNav'>NOSOTROS</Nav.Link>
+                                <Nav.Link href='#link' className='buttonNav'>PRODUCTOS</Nav.Link>
+                                <Nav.Link href='#link' className='buttonNav'>PROMOCIONES</Nav.Link>
+                                <Nav.Link href='#link' className='buttonNav'>LOCALES</Nav.Link>
+                                <Nav.Link href='#link' className='buttonNav'>CONTACTO</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Col>
+
+                </Row>
+
             </Container>
         </Navbar>
     );
