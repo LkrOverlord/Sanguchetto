1702e76428a82981d077d55106a357b2683d60e3
LkrOverlord <menesestapia@gmail.com>
2023-08-21 13:30:50 -0300

    Se agrega el NavBar Base y un boton que se uso para probar el react bootstrap

---
+++ src/components/TestComponent/ButtonTest.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+import './ButtonTest.css';
+
+const ButtonTest = () => {
+    return (
+        <div className="ButtonTest">
+            <Button variant="primary">Click me</Button>
+        </div>
+    );
+}
+
+export default ButtonTest;
